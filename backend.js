4550040746718635f9d0662b6f50ce9a16c3c8d4
Huyhihi2010 <89264311+Huyhihi2010@users.noreply.github.com>
2023-05-20 17:19:29 +0700

    Add files via upload

---
+++ backend.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+
+// socket.io setup
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000 })
+
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+const backEndPlayers = {}
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  backEndPlayers[socket.id] = {
+    x: 500 * Math.random(),
+    y: 500 * Math.random(),
+    color: `hsl(${360 * Math.random()}, 100%, 50%)`
+  }
+
+  io.emit('updatePlayers', backEndPlayers)
+
+  socket.on('disconnect', (reason) => {
+    console.log(reason)
+    delete backEndPlayers[socket.id]
+    io.emit('updatePlayers', backEndPlayers)
+  })
+
+  console.log(backEndPlayers)
+})
+
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+console.log('server did load')
